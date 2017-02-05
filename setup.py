64229778ed590e22c81a233cc8dd52d4dc55eae1
Yi Chen <xingyi998@Yis-MacBook-Pro.local>
2017-02-05 15:23:18 -0800

    add celery as dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     packages=['dyna_veggie'],  # this must be the same as the name above
     install_requires=[
         'retrying>=1.3.3',
-        'boto3>=1.4.4'
+        'boto3>=1.4.4',
+        'celery>=4.0.0'
     ],
     version='0.1',
     description='Fully compatible Celery result backend using DynamoDB',
@@ -13,7 +14,7 @@ setup(
     author_email='yi.chen.it@gmail.com',
     url='https://github.com/chensjlv/dyna-veggie',  # use the URL to the github repo
     # I'll explain this in a second
-    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',
+    download_url='https://github.com/chensjlv/dyna-veggie/tarball/0.1',
     keywords=['Celery', 'DynamoDB', 'backend'],  # arbitrary keywords
     classifiers=[],
 )
