f033b456a0dcc82ef941de2e32721f18f0127bca
Mike Chen <yi.chen.it@gmail.com>
2017-08-08 16:02:33 -0700

    upgrade to 0.3.6

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
         'boto3>=1.4.4',
         'celery>=4.0.0'
     ],
-    version='0.3.5',
+    version='0.3.6',
     description='Fully compatible DynamoDB result backend for Celery',
     long_description=open('README').read(),
     author='Mike Chen',
     author_email='yi.chen.it@gmail.com',
     url='https://github.com/chensjlv/dyna-veggie',  # use the URL to the github repo
     # I'll explain this in a second
-    download_url='https://github.com/chensjlv/dyna-veggie/tarball/0.3.5',
+    download_url='https://github.com/chensjlv/dyna-veggie/tarball/0.3.6',
     keywords=['Celery', 'DynamoDB', 'backend'],  # arbitrary keywords
     classifiers=[],
 )
